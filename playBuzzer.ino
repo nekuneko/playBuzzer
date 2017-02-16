d0c09c6be69dde148e4fc1a745935f26ce053a55
nekuneko <noct@Melchior.local>
2017-02-17 00:58:46 +0100

    Cambio en la arquitectura de notas
    Ahora las notas están estructuradas en vectores en lugar de en
    #defines. Existen 7 escalas de notas numeradas de 0 a 6, normalmente
    las canciones se tocan sobre una escala base y puntualmente se pulsan
    notas de una escala superior o inferior. Por este motivo, no es
    recomendable utilizar como escala base la 0 o la 6, ya que podría
    producirse un desbordamiento del vector.

--- playBuzzer.ino
+++ playBuzzer.ino
@@ -7,96 +7,26 @@
  *  - Terminar las canciones con silencio(blanca);
  * 
  * @author  Javier Villaverde Ramallo
- * @fecha   17 de Octubre de 2015
- * @version 1.0
+ * @fecha   17 de Febrero de 2016
+ * @version 2.0
  */
- 
-// Notas musicales.
+
+// Tenemos 7 escalas de notas, numeradas de 0 a 6
+uint16_t DO[7]    = {33,  65, 131, 262, 523, 1047, 2093};
+uint16_t DO_[7]   = {35,  69, 139, 277, 554, 1109, 2217};
+uint16_t RE[7]    = {37,  73, 147, 294, 587, 1175, 2349};
+uint16_t RE_[7]   = {39,  78, 156, 311, 622, 1245, 2489};
+uint16_t MI[7]    = {41,  82, 165, 330, 659, 1319, 2637};
+uint16_t FA[7]    = {44,  87, 175, 349, 698, 1397, 2794};
+uint16_t FA_[7]   = {46,  93, 185, 370, 740, 1480, 2960};
+uint16_t SOL[7]   = {49,  98, 196, 392, 784, 1568, 3136};
+uint16_t SOL_[7]  = {52, 104, 208, 415, 831, 1661, 3322};
+uint16_t LA[7]    = {55, 110, 220, 440, 880, 1760, 3520};
+uint16_t LA_[7]   = {58, 117, 233, 466, 932, 1865, 3729};
+uint16_t SI[7]    = {62, 123, 247, 494, 988, 1976, 3951};
+
+// Notas musicales restantes
 #define SI0   31
-#define DO1   33
-#define DOS1  35
-#define RE1   37
-#define RES1  39
-#define MI1   41
-#define FA1   44
-#define FAS1  46
-#define SOL1  49
-#define SOLS1 52
-#define LA1   55
-#define LAS1  58
-#define SI1   62
-#define DO2   65
-#define DOS2  69
-#define RE2   73
-#define RES2  78
-#define MI2   82
-#define FA2   87
-#define FA    93
-#define SOL2  98
-#define SOLS2 104
-#define LA2   110
-#define LAS2  117
-#define SI2   123
-#define DO3   131
-#define DOS3  139
-#define RE3   147
-#define RES3  156
-#define MI3   165
-#define FA3   175
-#define FAS3  185
-#define SOL3  196
-#define SOLS3 208
-#define LA3   220
-#define LAS3  233
-#define SI3   247
-#define DO4   262
-#define DOS4  277
-#define RE4   294
-#define RES4  311
-#define MI4   330
-#define FA4   349
-#define FAS4  370
-#define SOL4  392
-#define SOLS4 415
-#define LA4   440
-#define LAS4  466
-#define SI4   494
-#define DO5   523
-#define DOS5  554
-#define RE5   587
-#define RES5  622
-#define MI5   659
-#define FA5   698
-#define FAS5  740
-#define SOL5  784
-#define SOLS5 831
-#define LA5   880
-#define LAS5  932
-#define SI5   988
-#define DO6   1047
-#define DOS6  1109
-#define RE6   1175
-#define RES6  1245
-#define MI6   1319
-#define FA6   1397
-#define FAS6  1480
-#define SOL6  1568
-#define SOLS6 1661
-#define LA6   1760
-#define LAS6  1865
-#define SI6   1976
-#define DO7   2093
-#define DOS7  2217
-#define RE7   2349
-#define RES7  2489
-#define MI7   2637
-#define FA7   2794
-#define FAS7  2960
-#define SOL7  3136
-#define SOLS7 3322
-#define LA7   3520
-#define LAS7  3729
-#define SI7   3951
 #define DO8   4186
 #define DOS8  4435
 #define RE8   4699
@@ -105,34 +35,38 @@
 // Altavoz
 const int speaker = 9;  // PIN9 --(PIEZO)-- GND
 
-// Escala del tiempo
-float escala = 1;
+// Velocidad de reproducción
+float tempo = 1.0;
+
+// Escala musical
+uint8_t escala = 5;
 
 // Duración de las notas (Milisegundos)
-float semicorchea = 62   * escala;
-float corchea     = 125  * escala;
-float negra       = 250  * escala;
-float blanca      = 500  * escala;
-float redonda     = 1000 * escala;
+float semicorchea = 62   * tempo;
+float corchea     = 125  * tempo;
+float negra       = 250  * tempo;
+float blanca      = 500  * tempo;
+float redonda     = 1000 * tempo;
+
 
 void setup() 
 {
   pinMode(speaker, OUTPUT);
 
-  //  tocar(LA5);
+  //tocar(LA[escala]);
   //delay(10000);
   
-  actualizarEscala(2.0);
-  //cancionTiempo();
+  actualizarTempo(2.0);
+  //cancionTiempo(escala);
   
-  actualizarEscala(1.2);
-  //cancionTormenta(); 
+  actualizarTempo(1.2);
+  //cancionTormenta(escala); 
 
-  actualizarEscala(1.5);
-  //greenSleeves();
+  actualizarTempo(1.5);
+  //greenSleeves(escala);
 
-  actualizarEscala(1.3);
-  //hockey();
+  actualizarTempo(1.3);
+  //hockey(escala);
   
   silencio(0);
 }
@@ -174,17 +108,20 @@ void silencio (float nota)
   else if (nota == blanca)
       delay(blanca);
   else if (nota == redonda)
-      delay(redonda);      
+      delay(redonda);
+  else
+    delay(nota);    
 }
 
 
-void actualizarEscala(float escala)
+void actualizarTempo (float tempo)
 {
-  semicorchea = 62   * escala;
-  corchea     = 125  * escala;
-  negra       = 250  * escala;
-  blanca      = 500  * escala;
-  redonda     = 1000 * escala;
+  // Duración de las notas (Milisegundos)
+  semicorchea = 62   * tempo;
+  corchea     = 125  * tempo;
+  negra       = 250  * tempo;
+  blanca      = 500  * tempo;
+  redonda     = 1000 * tempo;
 }
 
 
@@ -192,61 +129,61 @@ void actualizarEscala(float escala)
 // ----- Canciones -----
 
 // Canción del Tiempo
-void cancionTiempo ()
+void cancionTiempo (uint8_t escala)
 {
-  tocar(LA5);
+  tocar(LA[escala]);
   delay(negra);
 
-  tocar(RE5);
+  tocar(RE[escala]);
   delay(blanca);
 
   silencio(semicorchea);
   
-  tocar(FA5);
+  tocar(FA[escala]);
   delay(negra);
 
-  tocar(LA5);
+  tocar(LA[escala]);
   delay(negra);
 
-  tocar(RE5);
+  tocar(RE[escala]);
   delay(blanca);
 
   silencio(semicorchea);
   
-  tocar(FA5);
+  tocar(FA[escala]);
   delay(negra);
 
-  tocar(LA5);
+  tocar(LA[escala]);
   delay(corchea);
 
-  tocar(DO6);
+  tocar(DO[escala+1]);
   delay(corchea);
 
-  tocar(SI5);
+  tocar(SI[escala]);
   delay(negra);
 
-  tocar(SOL5);
+  tocar(SOL[escala]);
   delay(negra);
 
-  tocar(FA5);
+  tocar(FA[escala]);
   delay(corchea);
 
-  tocar(SOL5);
+  tocar(SOL[escala]);
   delay(corchea);
 
-  tocar(LA5);
+  tocar(LA[escala]);
   delay(negra);
 
-  tocar(RE5);
+  tocar(RE[escala]);
   delay(negra);
 
-  tocar(DO5);
+  tocar(DO[escala]);
   delay(corchea);
 
-  tocar(MI5);
+  tocar(MI[escala]);
   delay(corchea);
 
-  tocar(RE5);
+  tocar(RE[escala]);
   delay(blanca);
 
   silencio(blanca);
@@ -254,161 +191,161 @@ void cancionTiempo ()
 
 
 // Canción de la tormenta
-void cancionTormenta ()
+void cancionTormenta (uint8_t escala)
 {
     // re fa RE re fa RE
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(corchea);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(RE6);
+    tocar(RE[escala+1]);
     delay(blanca);
 
     silencio(semicorchea);
 
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(corchea);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(RE6);
+    tocar(RE[escala+1]);
     delay(blanca);
 
     silencio(semicorchea);
 
     // MI FA MI FA MI DO la
-    tocar(MI6);
+    tocar(MI[escala+1]);
     puntillo(negra);
 
-    tocar(FA6);
+    tocar(FA[escala+1]);
     delay(corchea);
 
-    tocar(MI6);
+    tocar(MI[escala+1]);
     delay(corchea);
 
-    tocar(FA6);
+    tocar(FA[escala+1]);
     delay(corchea);
 
-    tocar(MI6);
+    tocar(MI[escala+1]);
     delay(corchea);
 
-    tocar(DO6);
+    tocar(DO[escala+1]);
     delay(corchea);
 
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(blanca);
 
     silencio(semicorchea);
 
     // la re fa sol la
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(negra);
 
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(negra);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(SOL5);
+    tocar(SOL[escala]);
     delay(corchea);
 
-    tocar(LA5);
+    tocar(LA[escala]);
     puntillo(blanca);
 
     silencio(semicorchea);
 
     // la re fa sol la
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(negra);
 
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(negra);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(SOL5);
+    tocar(SOL[escala]);
     delay(corchea);
 
-    tocar(MI5);
+    tocar(MI[escala]);
     puntillo(blanca);
 
 // Mitad de la canción
 
     // RE FA RE RE FA RE
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(corchea);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(RE6);
+    tocar(RE[escala]);
     delay(blanca);
 
     silencio(semicorchea);
 
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(corchea);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(RE6);
+    tocar(RE[escala+1]);
     delay(blanca);
 
     silencio(semicorchea);
 
     // MI FA MI FA MI DO la
-    tocar(MI6);
+    tocar(MI[escala+1]);
     puntillo(negra);
 
-    tocar(FA6);
+    tocar(FA[escala+1]);
     delay(corchea);
 
-    tocar(MI6);
+    tocar(MI[escala+1]);
     delay(corchea);
 
-    tocar(FA6);
+    tocar(FA[escala+1]);
     delay(corchea);
 
-    tocar(MI6);
+    tocar(MI[escala+1]);
     delay(corchea);
 
-    tocar(DO6);
+    tocar(DO[escala+1]);
     delay(corchea);
 
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(blanca);
 
     silencio(semicorchea);
 
     // la re fa sol
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(negra);
 
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(negra);
 
-    tocar(FA5);
+    tocar(FA[escala]);
     delay(corchea);
 
-    tocar(SOL5);
+    tocar(SOL[escala]);
     delay(corchea);
 
     // la la re
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(blanca);
 
     silencio(semicorchea);
     
-    tocar(LA5);
+    tocar(LA[escala]);
     delay(negra);
 
-    tocar(RE5);
+    tocar(RE[escala]);
     delay(blanca);
 
     silencio(blanca);   
@@ -416,279 +353,279 @@ void cancionTormenta ()
 
 
 // Green Sleeves
-void greenSleeves ()
+void greenSleeves (uint8_t escala)
 {
-   tocar(LA5);
+   tocar(LA[escala+1]);
    delay(negra);
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(RE6);
+   tocar(RE[escala+1]);
    delay(negra);
 
-   tocar(MI6);
+   tocar(MI[escala+1]);
    puntillo(negra);
 
-   tocar(FA6);
+   tocar(FA[escala+1]);
    delay(corchea);
 
-   tocar(MI6);
+   tocar(MI[escala+1]);
    delay(negra);
 
-   tocar(RE6);
+   tocar(RE[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(SOL5);
+   tocar(SOL[escala]);
    puntillo(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(corchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    delay(blanca);
 
    silencio(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
    silencio(semicorchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    puntillo(negra);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    delay(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
    silencio(semicorchea);
 
    //7 ----------------------
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(blanca);
 
    silencio(semicorchea);
    
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    delay(negra);
 
-   tocar(MI5);
+   tocar(MI[escala]);
    delay(blanca);
 
    silencio(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(RE6);
+   tocar(RE[escala+1]);
    delay(negra);
 
-   tocar(MI6);
+   tocar(MI[escala+1]);
    puntillo(negra);
 
-   tocar(FA6);
+   tocar(FA[escala+1]);
    delay(corchea);
 
-   tocar(MI6);
+   tocar(MI[escala+1]);
    delay(negra);
 
-   tocar(RE6);
+   tocar(RE[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(SOL5);
+   tocar(SOL[escala]);
    puntillo(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(corchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
    
    // 13 -------------------
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    puntillo(negra);
 
    silencio(semicorchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    puntillo(negra);
 
-   tocar(FAS5);
+   tocar(FA_[escala]);
    delay(corchea);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    delay(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    puntillo(blanca);
 
    silencio(semicorchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    puntillo(blanca);
 
    silencio(corchea);
 
-   tocar(SOL6);
+   tocar(SOL[escala+1]);
    puntillo(blanca);
 
    silencio(semicorchea);
 
-   tocar(SOL6);
+   tocar(SOL[escala+1]);
    puntillo(negra);
 
-   tocar(FA6);
+   tocar(FA[escala+1]);
    delay(corchea);
 
-   tocar(MI6);
+   tocar(MI[escala+1]);
    delay(negra);
 
-   tocar(RE6);
+   tocar(RE[escala+1]);
    delay(blanca);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(SOL5);
+   tocar(SOL[escala]);
    puntillo(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(corchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
    silencio(semicorchea);
 
    // 21 -------------------
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    puntillo(negra);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    delay(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    delay(negra);
 
-   tocar(MI5);
+   tocar(MI[escala]);
    puntillo(blanca);
 
    silencio(corchea);
 
-   tocar(SOL6);
+   tocar(SOL[escala+1]);
    puntillo(blanca);
 
    silencio(corchea);
 
-   tocar(SOL6);
+   tocar(SOL[escala+1]);
    puntillo(negra);
 
-   tocar(FA6);
+   tocar(FA[escala+1]);
    delay(corchea);
 
-   tocar(MI6);
+   tocar(MI[escala+1]);
    delay(negra);
 
    silencio(semicorchea);
 
    // 27 ----------------
 
-   tocar(RE6);
+   tocar(RE[escala+1]);
    delay(blanca);
 
    silencio(semicorchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(SOL5);
+   tocar(SOL[escala]);
    puntillo(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(corchea);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(negra);
 
-   tocar(DO6);
+   tocar(DO[escala+1]);
    puntillo(negra);
 
-   tocar(SI5);
+   tocar(SI[escala]);
    delay(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    delay(negra);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    puntillo(negra);
 
    silencio(semicorchea);
    
-   tocar(FAS5);
+   tocar(FA_[escala]);
    delay(corchea);
 
-   tocar(SOLS5);
+   tocar(SOL_[escala]);
    delay(negra);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    puntillo(blanca);
 
    silencio(corchea);
 
-   tocar(LA5);
+   tocar(LA[escala]);
    puntillo(blanca);
 
    silencio(blanca);
@@ -696,25 +633,25 @@ void greenSleeves ()
 
 
 // Acorde final de órgano de hockey
-void hockey ()
+void hockey (uint8_t escala)
 {
-  tocar(SI4);
+  tocar(SI[escala]);
   delay(corchea);
 
-  tocar(MI5);
+  tocar(MI[escala+1]);
   delay(corchea);
 
-  tocar(SOLS5);
+  tocar(SOL_[escala+1]);
   delay(corchea);
 
-  tocar(SI5);
+  tocar(SI[escala+1]);
   delay(negra);
 
-  tocar(SOLS5);
+  tocar(SOL_[escala+1]);
   delay(corchea);
 
-  tocar(SI5);
+  tocar(SI[escala]);
   delay(blanca);
-  delay(corchea);
+  delay(semicorchea);
 }
 
